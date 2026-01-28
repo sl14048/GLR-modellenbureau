3e416954041e76de7307d82ed6758a8198416200
Danice Ouwerkerk <102460@glr.nl>
2026-01-28 20:53:15 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
+$servername = "http://localhost:63342/";
 $username = "102460";
 $password = "Beroeps123";
 $dbname = "Beroeps_SDOK";
