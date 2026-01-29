635490a57bf66abc0f87f7e680843c5d66f2f56c
102819 <102819@glr.nl>
2026-01-29 11:28:20 +0100

    ik heb image upload er in gezet

--- model_aanmaak.php
+++ model_aanmaak.php
@@ -1,6 +1,12 @@
 <?php
 
 require "config.php";
+//maak image map
+$map = "img";
+
+//maak een thumbnail map
+$tn_map = "thumbnails";
+$bestand = $_FILES['afbeelding'];
 
 $resultaat = "";
 $errors = "";
@@ -25,7 +31,31 @@ try {
         'email' => $email
     ]);
 
+    if (move_uploaded_file($bestand["tmp_name"],$map ."/". $bestand["name"]))
+    {
+        $resultaat .= $bestand["name"] . "staat nu in" . $map. "<br/>";
 
+        $info = getimagesize($map. "/". $bestand["name"]);
+
+        $origineel = imagecreatefromjpeg($map. "/". $bestand["name"]);
+
+        $thumb = imagecreatetruecolor(100, 100);
+
+        imagecopyresampled($thumb, $origineel, 0, 0, 0, 0, 100, 100, $info[0], $info[1]);
+
+    } else {
+
+        $resultaat = "er is iets fout gegaan bij het uploaden";
+    }
+
+    if(imagejpeg($thumb, $tn_map."/tn_". $bestand["name"], 100))
+    {
+
+        $resultaat .= "thumbnail gemaakt" .$tn_map. "/tn_". $bestand["name"] . "<br/>";
+
+//        include("views/toevoegen_resultaat_view.php");
+
+    }  else { $resultaat = "er is iets fout gegaan bij het thumbnail"; }
 
     if ($stmt->rowCount()) {
         header('Location: inlog_verwerk.php');
